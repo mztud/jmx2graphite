0c242c74058feed6881376e6bfaf0562392bcb93
Marco Ziebell <marco.ziebell@tu-dresden.de>
2025-02-12 09:59:52 +0100

    fix(jmx2graphite): commons-lang3

--- src/main/java/io/logz/jmx2graphite/GraphiteClient.java
+++ src/main/java/io/logz/jmx2graphite/GraphiteClient.java
@@ -7,8 +7,8 @@ import com.google.common.base.Joiner;
 import com.google.common.collect.Lists;
 import net.jodah.failsafe.Failsafe;
 import net.jodah.failsafe.RetryPolicy;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.exception.ExceptionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
